06b2a8d3d444d198fd539d3d67044f0f33e12b06
leehanbin <leehan0617@naver.com>
2015-06-12 10:52:58 +0900

    commit 2

--- Exam/src/exam/MainClass.java
+++ Exam/src/exam/MainClass.java
@@ -3,6 +3,8 @@ package exam;
 public class MainClass {
 	public static void main(String[] args) {
 		A a = new A();
+		B b = new B();
+		System.out.println(b.getClass().getName());
 		System.out.println(a.getClass().getName());
 	}
 }
